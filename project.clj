0c332b8e8d99f8589229707a54e626e98f07c131
Chris Zheng <czheng@atlassian.com>
2017-02-15 19:48:02 +1100

    upgraded to 0.1.5

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject im.chit/oren "0.1.1"
+(defproject im.chit/oren "0.1.5"
   :description "system and process monitoring for clojure"
   :url "https://github.com/zcaudate/oren"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.github.dblock/oshi-core "2.1"]
-                 [im.chit/hara.object "2.2.15"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.github.dblock/oshi-core "3.3"]
+                 [im.chit/hara.object "2.4.8"]])
--- src/oren/types.clj
+++ src/oren/types.clj
@@ -1,87 +1,230 @@
 (ns oren.types
   (:require [hara.object :as object]))
 
-(object/extend-maplike
+(def ^:dynamic *platform* (str (oshi.SystemInfo/getCurrentPlatformEnum)))
+
+;; Hardware Abstractions
+
+(object/map-like
 
  oshi.SystemInfo
- {:tag "system"}
-
- ;; Hardware Types
- oshi.hardware.HardwareAbstractionLayer
- {:tag "hardware"}
- oshi.hardware.platform.windows.WindowsHardwareAbstractionLayer
- {:tag "hardware"}
- oshi.hardware.platform.mac.MacHardwareAbstractionLayer
- {:tag "hardware"}
- oshi.hardware.platform.linux.LinuxHardwareAbstractionLayer
- {:tag "hardware"}
-
- ;; Display Types
- oshi.hardware.Display
- {:tag "display"}
- oshi.hardware.platform.windows.WindowsDisplay
- {:tag "display"}
- oshi.hardware.platform.mac.MacDisplay
- {:tag "display"}
- oshi.hardware.platform.linux.LinuxDisplay
- {:tag "display"}
-
- ;; Memory Types
- oshi.hardware.GlobalMemory
- {:tag "memory"}
- oshi.hardware.platform.windows.WindowsGlobalMemory
- {:tag "memory"}
- oshi.hardware.platform.mac.MacGlobalMemory
- {:tag "memory"}
- oshi.hardware.platform.linux.LinuxGlobalMemory
- {:tag "memory"}
-  
- ;; PowerSource Types
- oshi.hardware.PowerSource
- {:tag "power"}
- oshi.hardware.platform.windows.WindowsPowerSource
- {:tag "power"}
- oshi.hardware.platform.mac.MacPowerSource
- {:tag "power"}
- oshi.hardware.platform.linux.LinuxPowerSource
- {:tag "power"}
-
- ;; CPU Types
+ {:tag "system" :read :all}
+ oshi.hardware.Baseboard
+ {:tag "baseboard" :read :all}
  oshi.hardware.CentralProcessor
- {:tag "cpu"}
- oshi.hardware.platform.windows.WindowsCentralProcessor
- {:tag "cpu"}
- oshi.hardware.platform.mac.MacCentralProcessor
- {:tag "cpu"}
-  oshi.hardware.platform.linux.LinuxCentralProcessor
- {:tag "cpu"}
+ {:tag "cpu" :read :all}
+ oshi.hardware.Disks
+ {:tag "disks" :read :all}
+ oshi.hardware.Display
+ {:tag "display" :read :all}
+ oshi.hardware.Firmware
+ {:tag "firmware" :read :all}
+ oshi.hardware.GlobalMemory
+ {:tag "memory" :read :all}
+ oshi.hardware.HardwareAbstractionLayer
+ {:tag "hardware" :read :all}
+ oshi.hardware.Networks
+ {:tag "networks" :read :all}
+ oshi.hardware.PowerSource
+ {:tag "power" :read :all}
+ oshi.hardware.Sensors
+ {:tag "sensors" :read :all}
+ oshi.hardware.UsbDevice
+ {:tag "usb" :read :all}
 
- ;; FileStore Types
+ oshi.hardware.HWDiskStore
+ {:tag "disk" :read :all}
+ oshi.hardware.HWPartition
+ {:tag "partition" :read :all}
+ 
+ oshi.hardware.NetworkIF
+ {:tag "net" :read :all})
+
+(when (= *platform* "FREEBSD")
+  (eval '(object/map-like
+          oshi.hardware.platform.unix.freebsd.FreeBsdCentralProcessor
+          {:tag "cpu" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdDisks
+          {:tag "disks" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdDisplay
+          {:tag "display" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdFirmware
+          {:tag "firmware" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdGlobalMemory
+          {:tag "memory" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdHardwareAbstractionLayer
+          {:tag "hardware" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdNetworks
+          {:tag "networks" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdPowerSource
+          {:tag "power" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdSensors
+          {:tag "sensors" :read :all}
+          oshi.hardware.platform.unix.freebsd.FreeBsdUsbDevice
+          {:tag "usb" :read :all})))
+
+(when (= *platform* "LINUX")
+  (eval '(object/map-like
+          oshi.hardware.platform.linux.LinuxCentralProcessor
+          {:tag "cpu" :read :all}
+          oshi.hardware.platform.linux.LinuxDisks
+          {:tag "disks" :read :all}
+          oshi.hardware.platform.linux.LinuxDisplay
+          {:tag "display" :read :all}
+          oshi.hardware.platform.linux.LinuxFirmware
+          {:tag "firmware" :read :all}
+          oshi.hardware.platform.linux.LinuxGlobalMemory
+          {:tag "memory" :read :all}
+          oshi.hardware.platform.linux.LinuxHardwareAbstractionLayer
+          {:tag "hardware" :read :all}
+          oshi.hardware.platform.linux.LinuxNetworks
+          {:tag "networks" :read :all}
+          oshi.hardware.platform.linux.LinuxPowerSource
+          {:tag "power" :read :all}
+          oshi.hardware.platform.linux.LinuxSensors
+          {:tag "sensors" :read :all}
+          oshi.hardware.platform.linux.LinuxUsbDevice
+          {:tag "usb" :read :all})))
+
+(when (= *platform* "MACOSX")
+  (eval '(object/map-like
+          oshi.hardware.platform.mac.MacCentralProcessor
+          {:tag "cpu" :read :all}
+          oshi.hardware.platform.mac.MacComputerSystem
+          {:tag "sys" :read :all}
+          oshi.hardware.platform.mac.MacDisks
+          {:tag "disks" :read :all}
+          oshi.hardware.platform.mac.MacDisplay
+          {:tag "display" :read :all}
+          oshi.hardware.platform.mac.MacFirmware
+          {:tag "firmware" :read :all}
+          oshi.hardware.platform.mac.MacGlobalMemory
+          {:tag "memory" :read :all}
+          oshi.hardware.platform.mac.MacHardwareAbstractionLayer
+          {:tag "hardware" :read :all}
+          oshi.hardware.platform.mac.MacNetworks
+          {:tag "networks" :read :all}
+          oshi.hardware.platform.mac.MacPowerSource
+          {:tag "power" :read :all}
+          oshi.hardware.platform.mac.MacSensors
+          {:tag "sensors" :read :all}
+          oshi.hardware.platform.mac.MacUsbDevice
+          {:tag "usb" :read :all})))
+
+(when (= *platform* "SOLARIS")
+  (eval '(object/map-like
+          oshi.hardware.platform.unix.solaris.SolarisCentralProcessor
+          {:tag "cpu" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisDisks
+          {:tag "disks" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisDisplay
+          {:tag "display" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisFirmware
+          {:tag "firmware" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisGlobalMemory
+          {:tag "memory" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisHardwareAbstractionLayer
+          {:tag "hardware" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisNetworks
+          {:tag "networks" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisPowerSource
+          {:tag "power" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisSensors
+          {:tag "sensors" :read :all}
+          oshi.hardware.platform.unix.solaris.SolarisUsbDevice
+          {:tag "usb" :read :all})))
+
+(when (= *platform* "WINDOWS")
+  (eval '(object/map-like
+          oshi.hardware.platform.windows.WindowsCentralProcessor
+          {:tag "cpu" :read :all}
+          oshi.hardware.platform.windows.WindowsDisks
+          {:tag "disks" :read :all}
+          oshi.hardware.platform.windows.WindowsDisplay
+          {:tag "display" :read :all}
+          oshi.hardware.platform.windows.WindowsFirmware
+          {:tag "firmware" :read :all}
+          oshi.hardware.platform.windows.WindowsGlobalMemory
+          {:tag "memory" :read :all}
+          oshi.hardware.platform.windows.WindowsHardwareAbstractionLayer
+          {:tag "hardware" :read :all}
+          oshi.hardware.platform.windows.WindowsNetworks
+          {:tag "networks" :read :all}
+          oshi.hardware.platform.windows.WindowsPowerSource
+          {:tag "power" :read :all}
+          oshi.hardware.platform.windows.WindowsSensors
+          {:tag "sensors" :read :all}
+          oshi.hardware.platform.windows.WindowsUsbDevice
+          {:tag "usb" :read :all})))
+
+;; Software Abstractions
+
+(object/map-like
+
+ oshi.software.os.FileSystem
+ {:tag "file" :read :all}
  oshi.software.os.OSFileStore
- {:tag "fs"}
- oshi.software.os.windows.WindowsFileSystem
- {:tag "fs"}
- oshi.software.os.mac.MacFileSystem
- {:tag "fs"}
- oshi.software.os.linux.LinuxFileSystem
- {:tag "fs"}
- 
- ;; OS Types
+ {:tag "fs" :read :all}
  oshi.software.os.OperatingSystem
- {:tag "os"}
- oshi.software.os.windows.WindowsOperatingSystem
- {:tag "os"}
- oshi.software.os.mac.MacOperatingSystem
- {:tag "os"}
- oshi.software.os.linux.LinuxOperatingSystem
- {:tag "os"}
- 
- ;; Version Types
+ {:tag "os" :read :all}
  oshi.software.os.OperatingSystemVersion
- {:tag "info"}
- oshi.software.os.windows.WindowsOSVersionInfoEx
- {:tag "info"}
- oshi.software.os.mac.MacOSVersionInfoEx
- {:tag "info"}
- oshi.software.os.linux.LinuxOSVersionInfoEx
- {:tag "info"})
+ {:tag "version" :read :all}
+ oshi.software.os.OSProcess
+ {:tag "process" :read :all})
+
+(when (= *platform* "FREEBSD")
+  (eval '(object/map-like
+          oshi.software.os.unix.freebsd.FreeBsdFileSystem
+          {:tag "fs" :read :all}
+          oshi.software.os.unix.freebsd.FreeBsdOperatingSystem
+          {:tag "os" :read :all}
+          oshi.software.os.unix.freebsd.FreeBsdOSVersionInfoEx
+          {:tag "info" :read :all}
+          oshi.software.os.unix.freebsd.FreeBsdProcess
+          {:tag "process" :read :all})))
+
+(when (= *platform* "LINUX")
+  (eval '(object/map-like
+          oshi.software.os.linux.LinuxFileSystem
+          {:tag "fs" :read :all}
+          oshi.software.os.linux.LinuxOperatingSystem
+          {:tag "os" :read :all}
+          oshi.software.os.linux.LinuxOSVersionInfoEx
+          {:tag "info" :read :all}
+          oshi.software.os.linux.LinuxProcess
+          {:tag "process" :read :all})))
+
+(when (= *platform* "MACOSX")
+  (eval '(object/map-like
+          oshi.software.os.mac.MacFileSystem
+          {:tag "fs" :read :all}
+          oshi.software.os.mac.MacOperatingSystem
+          {:tag "os" :read :all}
+          oshi.software.os.mac.MacOSVersionInfoEx
+          {:tag "info" :read :all}
+          oshi.software.os.mac.MacProcess
+          {:tag "process" :read :all})))
+
+(when (= *platform* "SOLARIS")
+  (eval '(object/map-like
+          oshi.software.os.unix.solaris.SolarisFileSystem
+          {:tag "fs" :read :all}
+          oshi.software.os.unix.solaris.SolarisOperatingSystem
+          {:tag "os" :read :all}
+          oshi.software.os.unix.solaris.SolarisOSVersionInfoEx
+          {:tag "info" :read :all}
+          oshi.software.os.unix.solaris.SolarisProcess
+          {:tag "process" :read :all})))
+
+(when (= *platform* "WINDOWS")
+  (eval '(object/map-like
+          oshi.software.os.windows.WindowsFileSystem
+          {:tag "fs" :read :all}
+          oshi.software.os.windows.WindowsOperatingSystem
+          {:tag "os" :read :all}
+          oshi.software.os.windows.WindowsOSVersionInfoEx
+          {:tag "info" :read :all}
+          oshi.software.os.windows.WindowsProcess
+          {:tag "process" :read :all})))
+
+
